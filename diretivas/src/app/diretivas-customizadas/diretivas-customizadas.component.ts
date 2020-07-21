f95b3f3f815011f9a3894c1a7cf20405564dc216
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2020-07-21 17:18:31 -0300

    Aulas de Diretivas Angular

--- diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-diretiva-ngif',
-  templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.scss']
+  selector: 'app-diretivas-customizadas',
+  templateUrl: './diretivas-customizadas.component.html',
+  styleUrls: ['./diretivas-customizadas.component.scss']
 })
-export class DiretivaNgifComponent implements OnInit {
+export class DiretivasCustomizadasComponent implements OnInit {
 
   cursos: string[] =['Angular'];
 
